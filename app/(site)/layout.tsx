f8e708f7873cc15c73850558e3f9a979ae31e86a
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-04-28 14:50:05 +0300

    deploy fix #1

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,9 +2,6 @@
 
 import Navbar from "../components/Navbar";
 import "../globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
       </body>
